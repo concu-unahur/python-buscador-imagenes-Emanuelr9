1beda48f4477d2de99cd903175669eb25af5b654
Emanuelr9 <emanuelr9@yahoo.com>
2020-02-18 21:02:21 -0300

    avanzando appi

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import logging
 from api import PixabayAPI
+import sys
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 carpeta_imagenes = './imagenes'
-query = 'computadoras'
-api = PixabayAPI('PONER ACÁ EL API KEY', carpeta_imagenes)
+query = sys.argv[1]
+cantidad= sys.argv[2]
 
-logging.info(f'Buscando imagenes de {query}')
-urls = api.buscar_imagenes(query, 5)
+
+api = PixabayAPI('15310694-79541d54c04910f8a3106dbd1', carpeta_imagenes)
+
+logging.info(f'Buscando {cantidad} imagenes de {query}')
+urls = api.buscar_imagenes(query, cantidad)
 
 for u in urls:
   logging.info(f'Descargando {u}')
-  api.descargar_imagen(u)
+  #api.descargar_imagen(u)
+
+threading.Thread(target=(lambda: api.descargar_imagen(u))).start()
